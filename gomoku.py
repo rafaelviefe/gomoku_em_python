a82f82c747556ac9832a911500706fe2cc0a67cd
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2024-06-09 02:54:42 -0300

    gomoku - 1.01

--- gomoku.py
+++ gomoku.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from time import sleep
 
 class Gomoku:
     def __init__(self, size=19, pvp_mode = 1, machine_turn = 0):
@@ -9,15 +9,17 @@ class Gomoku:
         self.moves = 0
         self.pvp_mode = pvp_mode
         self.machine_turn = machine_turn
-        self.x = 0
-        self.y = 0
+        self.x = size // 2
+        self.y = size // 2
     
     def print_board(self):
         i = 1
         print()
+        
         for row in self.board:
             if i < 10:
                 print("0", i, sep = "", end=" ")
+                
             else:
                 print(i, end=" ")
             i += 1
@@ -25,11 +27,12 @@ class Gomoku:
                 if column == 1:
                     print("●", end="   ")
                 elif column == 2:
-                    print("⓪", end="   ")
+                    print("⊙", end="   ")
                 else:
                     print("ᚔ", end="   ")
             print()
             print()
+            
         print("  ", end=" ")
         i = 1
         while i <= len(self.board):
@@ -42,8 +45,10 @@ class Gomoku:
             i += 1
         print()
         print()
+        sleep(1)
     
     def find_win(self):
+        '''com esse sistema também seria possível fazer com que a máquina fizesse jogadas menos randômicas'''
         directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
         for dx, dy in directions:
             count = 1
@@ -59,11 +64,18 @@ class Gomoku:
 
 
 def play_match():
-    
     pvp_mode = int(input('Jogador x Máquina (0) ou Jogador x jogador (1)? '))
+    while not 0 <= pvp_mode <= 1:
+        print('Entrada inválida!')
+        pvp_mode = int(input('Jogador x Máquina (0) ou Jogador x jogador (1)? '))
+        
     if not pvp_mode:
         machine_turn = int(input('Quem começa? Você (0) ou a Máquina (1)? '))
+        while not 0 <= machine_turn <= 1:
+            print('Entrada inválida!')
+            machine_turn = int(input('Quem começa? Você (0) ou a Máquina (1)? '))
         game = Gomoku(19, pvp_mode, machine_turn)
+        
     else:
         game = Gomoku(19, pvp_mode)
     game.print_board()
@@ -74,45 +86,52 @@ def play_match():
             game.current_player = 2
         else:
             game.current_player = 1
+            
         if game.machine_turn:
             print('Vez da máquina!')
             validation = False
             while not validation:
-                game.y = randint(0, 18)
-                game.x = randint(0, 18)
-                if not game.board[game.y][game.x]:
+                game.y += randint(-1, 1)
+                game.x += randint(-1, 1)
+                if 0 <= game.y <= 18 and 0 <= game.x <= 18 and not game.board[game.y][game.x]:
                     game.board[game.y][game.x] = game.current_player
-                    
+                    sleep(1)
                     print(f'A maquina jogou... {game.y+1} e {game.x+1}!')
+                    sleep(1)
                     print()
                     validation = True
             game.machine_turn = 0
+            
         else:
             if game.pvp_mode:
                 print(f'Vez do jogador {game.current_player}!')
             else:
                 print('Sua vez!')
+                
             validation = False
             while not validation:
                 game.y = int(input('Escolha a linha: ')) - 1
-                while game.y < 0 or game.y > 18:
-                    game.y = int(input('Posição inválida, tente novamente: ')) - 1 
+                while not 0 <= game.y <= 18:
+                    game.y = int(input('Posição inválida, tente novamente: ')) - 1
+                    
                 game.x = int(input('Escolha a coluna: ')) - 1
-                while game.x < 0 or game.x > 18:
-                    game.x = int(input('Posição inválida, tente novamente: ')) - 1 
+                while not 0 <= game.x <= 18:
+                    game.x = int(input('Posição inválida, tente novamente: ')) - 1
+                    
                 if game.board[game.y][game.x]:
                     print('Esta posição já foi preenchida! Tente outra...')
                 else:
+                    sleep(1)
                     print('Jogada Validada!')
+                    sleep(1)
                     print()
                     game.board[game.y][game.x] = game.current_player
                     validation = True
+                    
             if not game.pvp_mode:
                 game.machine_turn = 1
         game.moves += 1
         game.print_board()
-        print(game.machine_turn)
-
     if game.moves == 361:
         print('É... vocês conseguiram empatar.')
     elif game.pvp_mode:
@@ -123,7 +142,14 @@ def play_match():
         print('Não foi dessa vez, a máquina venceu...')
 
 
-play_match()
+def play_gomoku():
+    gaming = 1
+    sleep(1)
+    while gaming:
+        play_match()
+        sleep(1)
+        gaming = int(input('Parar (0) ou continuar jogando (1)? '))
+    print('Sessão encerrada! Obrigado por jogar.')
 
-
-
+print('Iniciando jogo...')
+play_gomoku()
